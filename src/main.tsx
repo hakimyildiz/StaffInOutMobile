905ef93629adbd643e7fbf569083ddea1f063158
hakim yildiz <simsiyah@hotmail.com>
2025-06-12 08:16:58 +0300

    all

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import { name as appName } from '../package.json';
 
 // Register the main application component
 AppRegistry.registerComponent(appName, () => App);
