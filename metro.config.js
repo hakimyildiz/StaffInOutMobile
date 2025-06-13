df1aae33716282703fb0de83d121b6e8b929a2ab
hakim yildiz <simsiyah@hotmail.com>
2025-06-13 17:44:37 +0300

    all

--- metro.config.js
+++
@@ -1,6 +0,0 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const defaultConfig = getDefaultConfig(__dirname);
-
-module.exports = mergeConfig(defaultConfig, {
-  // Add any custom Metro configuration here if needed
-});
